39e4e84f3c7a292b313ac95ce47cb6b563ff51db
Matthew Holland <matthew.holland@cmd.ox.ac.uk>
2024-02-08 19:55:16 +0000

    Added Parallel HCIE Search
    Added functionality using concurrent.futures library to enable parallel searching of VEHICLe using HCIE. This involves introduction of align_and_score_probe function which aligns individual probes to the query, and a database_search_parallel and vehicle_search_parallel functions.

--- hcie/database_search.py
+++ hcie/database_search.py
@@ -3,6 +3,7 @@ import json
 from pathlib import Path
 from datetime import datetime
 from rdkit import Chem
+from concurrent.futures import ThreadPoolExecutor
 
 import hcie
 from hcie import Molecule, Alignment
@@ -15,6 +16,42 @@ smiles_json_path = Path(data_dir_path) / "vehicle_smiles.json"
 # Load required data
 with open(smiles_json_path, 'r') as json_file:
     vehicle_dict = json.load(json_file)
+
+
+def vehicle_search_parallel(query_smiles: str,
+                   query_mol_vector: (int, int),
+                   query_name: str = None,
+                   num_of_output_mols: int = 20
+                   ):
+    """
+    Search a query molecule against the molecules in the VEHICLe database, aligning to the MedChem vector designated by
+    query_mol_vector_id. To determine the appropriate vector it is necessary to run query_mol.write_vectors_to_image()
+    and examine the image produced.
+
+
+    Parameters
+    ----------
+    query_name: Name of molecule, optional.
+    query_smiles: SMILES string of molecule to search against the VEHICLe database.
+    query_mol_vector: Functionalisable bond to align to (non-H atom ID, H-atom ID)
+    num_of_output_mols: number of mols to output to sdf file. Defaults to 20
+
+    Returns
+    -------
+    results:
+    aligned_mols
+    """
+    query_mol = Molecule(query_smiles, name=query_name)
+    aligned_mols = {}
+
+    aligned_mols['query_mol'] = query_mol
+
+    results = database_search_parallel(query_mol,
+                                       query_mol_vector=query_mol_vector,
+                                       probe_dict=vehicle_dict,
+                                       database_mols=aligned_mols)
+
+    return results, aligned_mols
 
 
 def vehicle_search(query_smiles: str,
@@ -117,6 +154,105 @@ def database_search(query_mol: hcie.Molecule,
 
     return sorted(results_list, key=lambda x: x[1], reverse=True)
 
+
+def align_and_score_probe(probe_regid: str,
+                          probe_smiles: str,
+                          query_mol: hcie.Molecule,
+                          query_mol_vector: (int, int),
+                          database_mols: dict = None,
+                          similarity_metric: str = 'tanimoto'
+                          ):
+    """
+    Aligns a probe molecule onto the query molecule along each of the functionalisable vectors in the probe molecule,
+    scores each, and saves each alignment with their scores. Returns the highest score.
+    Parameters
+    ----------
+    probe_regid: VEHICLe regid of probe molecule.
+    probe_smiles: SMILES string of probe molecule.
+    query_mol: HCIE.molecule of query molecule.
+    query_mol_vector: Vector in form (non-H atom idx, H atom idx) of query vector to align to.
+    database_mols: dictionary of database molecules to append to. Keys are regids, values are hcie.mols
+    similarity_metric: Metric to use for computing ESP similarity - defaults to Tanimoto.
+
+    Returns
+    -------
+    (probe_regid, best_score, best_idx, best_esp, best_shape)
+    """
+    probe = Molecule(probe_smiles, name=str(probe_regid))
+
+    if database_mols is not None:
+        database_mols[probe_regid] = probe
+
+    probe_vectors = probe.functionalisable_bonds
+
+    best_score = best_esp = best_shape = 0
+    best_idx = None
+
+    print(probe_regid)
+    for conf_idx, vector in enumerate(probe_vectors):
+        # Need to double the conf_idx because each alignment generates two conformers - the aligned one, and its 180-
+        # degree rotation
+        alignment = Alignment(probe_molecule=probe,
+                              reference_molecule=query_mol,
+                              reference_bond_idxs=query_mol_vector,
+                              probe_bond_idxs=vector,
+                              probe_conf_id=2*conf_idx)
+        _, _ = alignment.align_score(similarity_metric=similarity_metric)
+
+        for idx in (2*conf_idx, 2*conf_idx+1):
+
+            total_score = probe.total_scores[idx]
+
+            if total_score > best_score:
+                best_score = total_score
+                best_esp = probe.esp_scores[idx]
+                best_shape = probe.shape_scores[idx]
+                best_idx = idx
+
+    if best_idx is not None:
+        return probe_regid, best_score, best_idx, best_esp, best_shape
+
+
+def database_search_parallel(query_mol: hcie.Molecule,
+                    query_mol_vector: (int, int),
+                    probe_dict: dict,
+                    database_mols: dict,
+                    similarity_metric: str = 'tanimoto'
+                    ):
+    """
+
+    Parameters
+    ----------
+    query_mol: The molecule to use as a reference molecule for the search
+    query_mol_vector: Functionalisable bond to align to (non-H atom ID, H-atom ID)
+    probe_dict: dictionary of probe molecule identifiers (keys) and SMILES strings (values)
+    database_mols: a blank dictionary to which probe molecule identifiers (keys) and hcie molecules (values) will be
+                    appended.
+    similarity_metric: Similarity metric to use when scoring ESP similarity - defaults to Tanimoto
+
+    Returns
+    -------
+    Results: a list of tuples of results in the form (probe_id, best_score, best_idx, best_esp, best_shape) in order of
+    best combined score to worst.
+    """
+
+    results_list = []
+
+    # Use ThreadPoolExecutor to parallelise processing of VEHICLe.
+    with ThreadPoolExecutor() as executor:
+        futures = [executor.submit(align_and_score_probe,
+                                   entry[0],
+                                   entry[1],
+                                   query_mol,
+                                   query_mol_vector,
+                                   database_mols) for entry in probe_dict.items()]
+
+        for future in futures:
+            result = future.result()
+            if result:
+                results_list.append(result)
+
+    return sorted (results_list, key=lambda x: x[1], reverse=True)
 
 def new_directory(new_dir):
     def decorator(func):
