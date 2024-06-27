e1b947ca2cf5149eb83d211cb92898edfed2f28a
Matthew Holland <matthew.holland@cmd.ox.ac.uk>
2024-06-27 17:27:39 +0100

    Updated __init__.py and setup.py with version information
    Improved __init__.py and setup.py to include information about the author

--- hcie/__init__.py
+++ hcie/__init__.py
@@ -1,2 +1,6 @@
 from hcie.classes import Molecule, Alignment
 from hcie.database_search import vehicle_search_parallel
+
+__version__ = '0.1.2'
+__author__ = 'Matthew Holland'
+__credits__ = 'Centre For Medicines Discovery, University of Oxford'
