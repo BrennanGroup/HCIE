1de86a6e11c82b90df42043e9c07d97d4f3f432c
Matthew Holland <matthew.holland@cmd.ox.ac.uk>
2025-08-13 11:11:32 +0100

    Change library path in vehicle_search
    Updated the library .json files that are loaded for searching in vehicle_search.py

--- hcie/vehicle_search.py
+++ hcie/vehicle_search.py
@@ -11,14 +11,14 @@ from hcie.outputs import print_results, alignments_to_sdf, mols_to_image
 # Load full database into memory
 def load_database():
     with importlib.resources.files("Data").joinpath(
-        "bifunctionalised_database_no_peb.json"
+        "MoBiVic_2.json"
     ).open("r") as json_file:
         data = json.load(json_file)
     return data
 
 
 # Load dictionary looking up molecules by hash
-with importlib.resources.files("Data").joinpath("database_by_hash_no_pebs.json").open(
+with importlib.resources.files("Data").joinpath("mobivic_by_hash.json").open(
     "r"
 ) as json_file:
     vehicle_by_hash = json.load(json_file)
